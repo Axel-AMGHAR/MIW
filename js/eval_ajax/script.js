5547db45a667928e52f8528c2ad0e4619e7209a7
Axel-AMGHAR <axel.amghar@etu.univ-amu.fr>
2019-12-04 19:29:01 +0100

    création de deux tableaux pour la pagination

--- js/eval_ajax/script.js
+++ js/eval_ajax/script.js
@@ -15,14 +15,33 @@ onload = function () {
 <input name="livres" type="radio" id="non_dispo">
 <label for="non_dispo">Livres non disponibles</label></div>`;
 
-        function changePage(page){
-            let prev = document.getElementById('prev');
-            let next = document.getElementById('next');
+        function dis_trs(livre, bool,chaine){
+            for (let i=0; i< livre.length; i++){
+                if (livre[i].getElementsByTagName('disponible')[0].textContent == bool){
+                    if (i<10)
+                        chaine += `<tr id="${i}">`;
+                    else
+                        chaine += `<tr style="display:none" id="${i}">`;
+                    chaine += `<td>${livre[i].getElementsByTagName('titre')[0].textContent}</td>`
+                    chaine += `<td>${livre[i].getElementsByTagName('auteur')[0].textContent}</td>`;
+                    chaine += `<td>${livre[i].getElementsByTagName('genre')[0].textContent}</td>`;
+                    chaine += `<td>${livre[i].getElementsByTagName('editeur')[0].textContent}</td>`;
+                    chaine += `<td >${livre[i].getElementsByTagName('disponible')[0].textContent}</td></tr>`;
                 }
+            }
+            return chaine;
+        }
 
-            let chaine = 
-                `<table>
-<thead>
+        for (let i=0; i<2; i++){
+
+
+            let chaine = '';
+            if (i%2 == 0)
+                chaine += `<table id="oui">`;
+            else
+                chaine += `<table style="display_none" id="non">`;
+
+            chaine += `<thead>
 <tr>
 <th>Titre</th>
 <th>Auteur</th>
@@ -34,48 +53,38 @@ onload = function () {
 <tbody style='text-align:center'>`;
 
             let livre = txt.getElementsByTagName('livre');
-            for (let i=0; i< livre.length; i++){
-                if (livre[i].getElementsByTagName('disponible')[0].textContent == 'oui'){
 
-                    chaine += `<tr class="oui"><td>${livre[i].getElementsByTagName('titre')[0].textContent}</td>`;
-                    chaine += `<td>${livre[i].getElementsByTagName('auteur')[0].textContent}</td>`;
-                    chaine += `<td>${livre[i].getElementsByTagName('genre')[0].textContent}</td>`;
-                    chaine += `<td>${livre[i].getElementsByTagName('editeur')[0].textContent}</td>`;
-                    chaine += `<td >${livre[i].getElementsByTagName('disponible')[0].textContent}</td></tr>`;
-                }
-                else{
+            if (i%2 == 0)
+                chaine += dis_trs(livre,'oui',chaine);
+            else
+                chaine += dis_trs(livre,'non',chaine);
 
-                    chaine += `<tr class="non" style='display: none;'><td>${livre[i].getElementsByTagName('titre')[0].textContent}</td>`;
-                    chaine += `<td>${livre[i].getElementsByTagName('auteur')[0].textContent}</td>`;
-                    chaine += `<td>${livre[i].getElementsByTagName('genre')[0].textContent}</td>`;
-                    chaine += `<td>${livre[i].getElementsByTagName('editeur')[0].textContent}</td>`;
-                    chaine += `<td>${livre[i].getElementsByTagName('disponible')[0].textContent}</td></tr>`;
-                }
-            }
-
-            chaine += `</tbody>
-</table>`;
-            document.getElementById('tab_livres').innerHTML = chaine;
-
-            document.getElementById('disp').onclick = function (){
-                Array.from(document.getElementsByClassName('oui')).forEach((el) => {
-                    el.style.display = 'table-row';
-                });
-
-                Array.from(document.getElementsByClassName('non')).forEach((el) => {
-                    el.style.display = 'none';
-                });
-            };
-
-            document.getElementById('n_disp').onclick = function (){
-                Array.from(document.getElementsByClassName('oui')).forEach((el) => {
-                    el.style.display = 'none';
-                });
-
-                Array.from(document.getElementsByClassName('non')).forEach((el) => {
-                    el.style.display = 'table-row';
-                });
-            };
-
+            chaine += `</tbody></table>`;
+            
+            console.log(chaine);
+            document.getElementById('tab_livres'+i).innerHTML = chaine;
         }
-    };
+
+
+//        document.getElementById('disp').onclick = function (){
+//            Array.from(document.getElementsByClassName('oui')).forEach((el) => {
+//                el.style.display = 'table-row';
+//            });
+//
+//            Array.from(document.getElementsByClassName('non')).forEach((el) => {
+//                el.style.display = 'none';
+//            });
+//        };
+//
+//        document.getElementById('n_disp').onclick = function (){
+//            Array.from(document.getElementsByClassName('oui')).forEach((el) => {
+//                el.style.display = 'none';
+//            });
+//
+//            Array.from(document.getElementsByClassName('non')).forEach((el) => {
+//                el.style.display = 'table-row';
+//            });
+//        };
+
+    }
+};
