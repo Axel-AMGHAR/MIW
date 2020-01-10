763b9973661cd4a0404c429d81ae620db01de1e7
Axel-AMGHAR <axel.amghar@etu.univ-amu.fr>
2020-01-10 15:53:37 +0100

    Node js / avancement du projet yuka

---
+++ js/nuxt/pages/product/_id.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        {{id}}
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted(){
+            this.id = this.$route.params.id
+        },
+        
+        data : function(){
+            return {id : ''}
+        }
+    }
+</script>
--- js/nuxt/pages/products.vue
+++ js/nuxt/pages/products.vue
@@ -1,18 +1,45 @@
 <template>
-  <div>test</div>
+  <div class="products">
+    <qrcode-stream @decode="onDecode"></qrcode-stream>
+    <qrcode-drop-zone @decode="onDecode"></qrcode-drop-zone>
+    <qrcode-capture @decode="onDecode"></qrcode-capture>
+    <product
+      v-for="(product, i) in products"
+      :key="i"
+      class="product"
+      :product="product"
+    ></product>
+  </div>
 </template>
 
 <script>
+import Product from '../components/Product'
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
 import { mapActions, mapState } from 'vuex'
+
 export default {
   methods: {
+    onDecode: function(result) {
+      const matchingProduit = this.products.find(
+        product => product.bar_code === parseInt(result)
+      )
+      this.$router.push({
+        path: matchingProduit ? `product/${matchingProduit._id}` : '404'
+      })
+    },
     ...mapActions(['fetchProducts'])
   },
   mounted() {
     this.fetchProducts()
   },
+  components: {
+    QrcodeStream,
+    QrcodeDropZone,
+    QrcodeCapture,
+    Product
+  },
   computed: mapState({
-    products: state => state.api
+    products: state => state.products
   })
 }
 </script>
