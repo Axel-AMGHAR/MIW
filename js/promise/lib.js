af63f11d4292cc62d1936e86e3ba689823427251
Axel-AMGHAR <axel.amghar@etu.univ-amu.fr>
2020-01-17 00:49:25 +0100

    lib functions : append/prepend

--- js/promise/lib.js
+++ js/promise/lib.js
@@ -1,8 +1,20 @@
 /**
 * TO DO
 *
-*  fonction each 
+* each()
+* first()/last() element of NodeList
+* text() without parameter should give only the textcontent and not the html content
+* see text(el) with a parameter
+* see textContent js
+* finish after() and before()
+* css() handle the case to convert background-color into backgroundColor
+* see conputed style ( when the css is in other file)
 */
+
+/*??
+each : function(func){
+    func(index);
+}*/
 
 /**
 *   Copy functions to a prototype 
@@ -22,20 +34,20 @@ function extend(objDest,objSourc){
 */
 function $(selector){
     if (selector.substr(0,1) == '<'){
-        // If we want to create an element
-        const regex = /\w+/g;
-        const element = selector.match(regex)[0];
-        let element_created = document.createElement(element);
-        const attributes = selector.match(regex);
-        for (let i = 1; i < attributes.length ; i+=2 )
-            element_created.setAttribute(attributes[i], attributes[i+1]);
-        return element_created;
+        /** If we want to create an element **/
+        let regex = /\//
+        if (!regex.test(selector))
+            return console.error('Missing character /')
+        let element = document.createElement('span');
+        element.innerHTML = selector;
+        return element.children[0];
     } else{
-        // If we want to select one or more elements
+        /** If we want to select one or more elements **/
         return (document.querySelectorAll(selector).length == 1)?document.querySelector(selector):document.querySelectorAll(selector);
-
     }  
 }
+
+/** Node **/
 
 extend(Node.prototype,{
     /**
@@ -59,6 +71,34 @@ extend(Node.prototype,{
         return this;
     },
     /**
+    *   Get the css value of a Node
+    * @param  {String} arguments[0] - a css property
+    * @return {String} - the css value
+    *
+    *   Set one or more css properties of a Node
+    * @param {Object} arguments[0] - Object{ property: new_value }
+    * @return {Node}
+    *
+    *   Set one css value of a Node
+    * @param {String} argument[0] - a css property
+    * @param {String} argument[0] - the new value   
+    * @return {Node}
+    */
+    css : function(){
+        if (typeof arguments[0] == 'object')
+            for (let item_obj in arguments[0])
+                this.style[item_obj] = arguments[0][item_obj];
+        else if (arguments.length == 2)
+            this.style[arguments[0]] = arguments[1];
+        else if (arguments.length == 1){
+            if (this.style[arguments[0]] == undefined)
+                return console.error('Argument : [' + arguments[0] + '] doesn\'t exist');
+            return this.style[arguments[0]];
+        } else
+            console.error('0 or more than 2 arguments');
+        return this;
+    },
+    /**
     *   Add a node or a text at the end of a Node
     *
     * @param  {String|Node} element    
@@ -77,8 +117,20 @@ extend(Node.prototype,{
     prepend_ : function(element){
         this.prepend(element);
         return this;
+    },
+    /**
+    *   Add a node or a text after this Node
+    *
+    * @param  {String|Node} element    
+    * @return {Node}   
+    */
+    after_ : function(element){
+        this.after(element);
+        return this;
     }
 });
+
+/** NodeList **/
 
 extend(NodeList.prototype,{
     /**
@@ -117,7 +169,6 @@ extend(NodeList.prototype,{
     * @return {NodeList}
     *
     *   Set one css value for each Nodes
-    *
     * @param {String} argument[0] - a css property
     * @param {String} argument[0] - the new value   
     * @return {NodeList}
@@ -154,7 +205,7 @@ extend(NodeList.prototype,{
     * @param  {String|Node} element
     * @return {NodeList}
     */
-    append : function(element){
+    append_ : function(element){
         for (let item of this){
             item.append(element);
         }
@@ -166,13 +217,17 @@ extend(NodeList.prototype,{
     * @param  {String|Node} element    
     * @return {NodeList}   
     */
-    prepend : function(element){
+    prepend_ : function(element){
         for (let item of this){
             item.prepend(element);
         }
         return this;
     },
-    
+    /**
+    *   Submit a NodeList
+    *  
+    * @return {NodeList}
+    */
     submit : function(){
         for (let item of this){
             item.submit();
@@ -180,8 +235,3 @@ extend(NodeList.prototype,{
         return this;
     }
 });
-
-/*??
-each : function(func){
-    func(index);
-}*/
